42e909164a14d487a850512f73f4109a244b2f39
vemolista <tomas@tomasvemola.com>
2025-06-06 22:54:46 +0200

    clean up

---
+++ repl.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		command := cleanInput(input)[0]
+
+		i, ok := getCommands()[command]
+		if !ok {
+			fmt.Print("Unknown command.")
+		}
+
+		i.callback()
+	}
+}
+
+func cleanInput(text string) []string {
+	text = strings.ToLower(text)
+	return strings.Fields(text)
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex.",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Display help message.",
+			callback:    commandHelp,
+		},
+	}
+}
--- repl_test.go
+++ repl_test.go
@@ -6,30 +6,37 @@ import (
 
 func TestCleanInput(t *testing.T) {
 	cases := []struct {
+		name     string
 		input    string
 		expected []string
 	}{
 		{
+			name:     "two words with white space",
 			input:    "  hello  world  ",
 			expected: []string{"hello", "world"},
 		},
 		{
+			name:     "empty string",
 			input:    "",
 			expected: []string{},
 		},
 		{
+			name:     "leading white space",
 			input:    "  hi",
 			expected: []string{"hi"},
 		},
 		{
+			name:     "trailing white space",
 			input:    "hi  ",
 			expected: []string{"hi"},
 		},
 		{
+			name:     "three words with white space",
 			input:    "  hi  ho  hey ",
 			expected: []string{"hi", "ho", "hey"},
 		},
 		{
+			name:     "capitalized",
 			input:    "HI",
 			expected: []string{"hi"},
 		},
@@ -39,8 +46,8 @@ func TestCleanInput(t *testing.T) {
 		actual := cleanInput(c.input)
 
 		if len(actual) != len(c.expected) {
-			t.Errorf("For input %q: lengths mismatch. Expected %d, got %d. Expected: %v, Actual: %v",
-				c.input, len(c.expected), len(actual), c.expected, actual)
+			t.Errorf("Test case %s... For input %q: lengths mismatch. Expected %d, got %d. Expected: %v, Actual: %v",
+				c.name, c.input, len(c.expected), len(actual), c.expected, actual)
 		}
 
 		for i := range actual {
