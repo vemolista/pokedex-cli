eb0ee22637d42671c853f433917ee456e553ef79
vemolista <tomas@tomasvemola.com>
2025-06-16 11:41:21 +0200

    Rename

---
+++ internal/poke_api/locations.go
@@ -0,0 +1,69 @@
+package poke_api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const baseUrl = "https://pokeapi.co/api/v2/"
+
+type ShallowLocationAreasResponse struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func (c *Client) GetLocationAreas(url *string) (ShallowLocationAreasResponse, error) {
+	fullUrl := baseUrl + "location-area/"
+	if url != nil {
+		fullUrl = *url
+	}
+
+	i, ok := c.cache.Get(fullUrl)
+	if ok {
+		var locationAreasResponse ShallowLocationAreasResponse
+		err := json.Unmarshal(i, &locationAreasResponse)
+
+		if err != nil {
+			return ShallowLocationAreasResponse{}, fmt.Errorf("error unmarshalling response body: %w", err)
+		}
+
+		return locationAreasResponse, nil
+	}
+
+	req, err := http.NewRequest("GET", fullUrl, nil)
+	if err != nil {
+		return ShallowLocationAreasResponse{}, fmt.Errorf("error creating request: %w", err)
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return ShallowLocationAreasResponse{}, fmt.Errorf("error requesting: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return ShallowLocationAreasResponse{}, fmt.Errorf("API returned non-ok status: %s", res.Status)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return ShallowLocationAreasResponse{}, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	c.cache.Add(fullUrl, body)
+
+	var locationAreasResponse ShallowLocationAreasResponse
+	err = json.Unmarshal(body, &locationAreasResponse)
+	if err != nil {
+		return ShallowLocationAreasResponse{}, fmt.Errorf("error unmarshalling response body: %w", err)
+	}
+
+	return locationAreasResponse, nil
+}
